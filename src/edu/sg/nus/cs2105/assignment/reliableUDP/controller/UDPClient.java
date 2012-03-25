4a22071b8b9bac3e3b8ee8e1af950177f504f15d
Stephenbanana <stephenbanana.yilin@gmail.com>
2012-03-25 16:07:44 +0800

    Finishing UDPClient

--- src/edu/sg/nus/cs2105/assignment/reliableUDP/controller/UDPClient.java
+++ src/edu/sg/nus/cs2105/assignment/reliableUDP/controller/UDPClient.java
@@ -15,7 +15,8 @@ public class UDPClient {
 	private DatagramSocket skt;
 //	private InetAddress server;
 //	int port;
-	int seq;
+	int currentSegNum;
+	int totalSegNum;
 	final int SEG_SIZE = 60000;
 	
 	public UDPClient() {
@@ -27,7 +28,7 @@ public class UDPClient {
 		}
 	}
 	
-	public boolean sendFile(String filePath, String addr, String port){
+	public boolean sendFile(String filePath, String addr, String port) throws IOException{
 		File theFile = new File(filePath);
 		InetAddress ad = null;
 		try {
@@ -36,11 +37,13 @@ public class UDPClient {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		byte[] fileContent = null;
+
+		FileInputStream fin = null;
+		if(fin==null)
 		try{
-			FileInputStream fin = new FileInputStream(theFile);
-			fileContent = new byte[(int)theFile.length()];
-			fin.read(fileContent);
+			fin = new FileInputStream(theFile);
+//			fileContent = new byte[(int)theFile.length()];
+//			fin.read(fileContent);
 		}
 		catch(FileNotFoundException e){
 			System.out.println("File not found"+e);
@@ -48,19 +51,21 @@ public class UDPClient {
 		catch (IOException ioe){
 			System.out.println("Exception while reading the file"+ioe);
 		}
+		
+//		initiate connection with server, sending info about the file.
+		initConnection(theFile, ad, Integer.parseInt(port));
+		
+		
 //		Assume that numOfSegs will be smaller than 128, indicating the file size is smaller than 128*60000 < 7GB.
-		int numOfSegs = fileContent.length/SEG_SIZE;
-		for(int i = 0; i <= numOfSegs;){
-			byte[] seg = Arrays.copyOfRange(fileContent, i*SEG_SIZE, i*SEG_SIZE-1);
+//		int numOfSegs = fileContent.length/SEG_SIZE;
+		byte[] fileContent = new byte[SEG_SIZE];
+		for(int i = 0; i < totalSegNum;){
+			fin.read(fileContent, i*SEG_SIZE, SEG_SIZE);
 			byte[] data = new byte[SEG_SIZE+2];
-			data[0] = (byte)i;
-			data[1] = (i == numOfSegs)?(byte)0:(byte)1;
-			seq = i;
-			System.arraycopy(seg, 0, data, 2, seg.length);
+			data[0] = (byte)i+1;
+			currentSegNum = i+1;
+			System.arraycopy(fileContent, 0, data, 1, fileContent.length);
 			DatagramPacket outPkt = new DatagramPacket(data, data.length,ad,Integer.parseInt(port));
-//			if(!sendPacket(outPkt)){
-//				sendPacket(outPkt);
-//			}
 			boolean result = sendPacket(outPkt);
 			if(!result)
 				result = sendPacket(outPkt);
@@ -69,6 +74,21 @@ public class UDPClient {
 		return false;
 	}
 	
+	private void initConnection(File theFile, InetAddress ad, int port) {
+		// TODO Auto-generated method stub
+		byte[] data = new byte[SEG_SIZE+2];
+		byte[] fileName = theFile.getName().getBytes();
+		totalSegNum = (int) Math.ceil(theFile.length()/SEG_SIZE);
+		data[0] = (byte)0;
+		data[1] = (byte)totalSegNum;
+		currentSegNum = 0;
+		System.arraycopy(fileName, 0, data, 2, fileName.length);
+		DatagramPacket outPkt = new DatagramPacket(data, data.length, ad, port);
+		boolean result = sendPacket(outPkt);
+		if(!result)
+			result = sendPacket(outPkt);			
+	}
+
 	private boolean sendPacket(DatagramPacket pkt){
 		byte inBuf[] = new byte[1];
 		DatagramPacket inPkt = new DatagramPacket(inBuf, 1);
@@ -82,7 +102,7 @@ public class UDPClient {
 			return false;
 		}
 		
-		if (inBuf[0] != (byte)seq)
+		if (inBuf[0] != (byte)currentSegNum)
 		return false;
 		
 		else return true;
